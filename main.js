6f8b4a1af0e6340b3d8a0c75d9077e5f8a4585cc
Celso Silva <silva.celso29@gmail.com>
2019-04-24 12:48:17 -0400

    added addDishToCourse and getRandomDishFromCourse methods

--- main.js
+++ main.js
@@ -17,5 +17,40 @@ menu = {
     this._courses.desserts = desserts;
   },
 
-  
+  set appetizers(appetizers) {
+    return this._courses.appetizers = appetizers;
+  },
+
+  set mains(mains) {
+    return this._courses.mains = mains;
+  },
+
+  set desserts(desserts) {
+    return this._courses.desserts = desserts;
+  },
+
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts
+
+    }
+  },
+
+  addDishToCourse(courseName, dishName, dishPrice) {
+    const dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    return this._courses[courseName].push(dish);
+  },
+
+  getRandomDishFromCourse(courseName) {
+    const dishes = this._courses[courseName]
+    const randomIndex = Math.floor(Math.random * dishes.length)
+    return dishes[randomIndex];
+  },
+
+
 };
